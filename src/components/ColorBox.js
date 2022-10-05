82b5b39a683bf91aea440ac9121ba04b8f19cecd
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-10-05 16:49:57 +0200

    fix bug when add card, card disappears

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,17 +1,42 @@
+import { useState } from "react";
 import "./ColorBox.css";
 
-export default function ColorBox({ colorId, color, onUpdate, onDelete }) {
+export default function ColorBox({ color, onUpdate, onDelete }) {
+  const [hasBeenCopied, setHasbeenCopied] = useState(false);
+  if (hasBeenCopied) {
+    setTimeout(() => setHasbeenCopied(false), 2000);
+  }
+
   function HandleClick() {
-    navigator.clipboard.writeText(color);
+    navigator.clipboard.writeText(color.colorCode);
+    setHasbeenCopied(true);
+  }
+
+  function handleUpdate(event) {
+    onUpdate(color.id, event.target.value);
   }
 
   return (
     <li
       className="color-box"
-      style={{ backgroundColor: color }}
+      style={{ backgroundColor: color.colorCode }}
       onClick={HandleClick}
     >
-      <p>{color}</p>
+      <input
+        value={color.colorCode}
+        onClick={(event) => event.stopPropagation()}
+        onChange={handleUpdate}
+      />
+      <button
+        className="delete-button"
+        onClick={(event) => {
+          event.stopPropagation();
+          onDelete();
+        }}
+      >
+        ✕
+      </button>
+      {hasBeenCopied && <p className="message">copied!</p>}
     </li>
   );
 }
