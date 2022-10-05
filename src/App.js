4b2c7292a4c28fae8ba3134b4f583a98ed5b90f0
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-10-05 16:34:06 +0200

    fix structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import ColorBox from "./components/ColorBox";
+import { nanoid } from "nanoid";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const colorList = [
+    {
+      id: nanoid(),
+      colorCode: "#CCC",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#FF0000",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#012345",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#fedcba",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <ul className="card-grid">
+        {colorList.map((color) => (
+          <ColorBox key={color.id} color={color.colorCode} colorId={color.id} />
+        ))}
+      </ul>
+    </main>
   );
 }
-
-export default App;
