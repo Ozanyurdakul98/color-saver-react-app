b69104d98f6a24399e33616cabbb9dd78bb05747
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-10-05 16:45:05 +0200

    fix add color to clipboard bug

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 import "./index.css";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
